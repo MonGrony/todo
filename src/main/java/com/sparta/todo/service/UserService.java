4d5a4f85a10b4c2ca11c1c6517c85ee499ecc92f
MonGroy <mongtualmr@gmail.com>
2024-05-31 12:10:04 +0900

    로그인 구현

--- src/main/java/com/sparta/todo/service/UserService.java
+++ src/main/java/com/sparta/todo/service/UserService.java
@@ -1,9 +1,12 @@
 package com.sparta.todo.service;
 
+import com.sparta.todo.dto.LoginRequestDto;
 import com.sparta.todo.dto.SignupRequestDto;
 import com.sparta.todo.entity.User;
 import com.sparta.todo.entity.UserRoleType;
+import com.sparta.todo.jwt.JwtUtil;
 import com.sparta.todo.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,10 +18,12 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     private final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
@@ -43,5 +48,21 @@ public class UserService {
 
         User user = new User(nickname, username, role, password);
         userRepository.save(user);
+    }
+
+    public void login(LoginRequestDto requestDto, HttpServletResponse response) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 아닙니다.")
+        );
+
+        if (!passwordEncoder.matches((password), user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        String token = jwtUtil.createToken(user.getUsername(), user.getRoleType());
+        jwtUtil.addJwtToCookie(token, response);
     }
 }
